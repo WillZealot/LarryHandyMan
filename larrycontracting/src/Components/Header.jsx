4fb1d4033d6ff8b1dfaf238bd1298288348269ff
William Tikhonenko <williamtikhonenko@gmail.com>
2023-12-23 15:40:51 -0500

    added some text looks bare but its whatever

--- larrycontracting/src/Components/Header.jsx
+++ larrycontracting/src/Components/Header.jsx
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import logo from '../img/tools.png';
 
 const Header = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-
   const listItemStyle = {
     transition: 'transform 0.3s ease-in-out',
   };
@@ -16,32 +14,12 @@ const Header = () => {
     event.target.style.transform = 'scale(1)';
   };
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollPosition(window.scrollY);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  const getBackgroundOpacity = () => {
-    // You can adjust the divisor to control the rate of opacity change
-    const opacity = 1 - scrollPosition / 800;
-    return opacity >= 0 ? opacity : 0;
+  const headerStyle = {
+    backgroundColor: 'white', // Set the background color to white
   };
 
   return (
-    <div
-      className="sticky top-0 z-50"
-      style={{
-        background: `linear-gradient(to bottom, rgba(255, 255, 255, ${getBackgroundOpacity()}), transparent)`,
-        transition: 'background 0.3s ease-in-out',
-      }}
-    >
+    <div className="sticky top-0 z-50" style={headerStyle}>
       <div className="flex items-center justify-between p-4">
         <div className="flex items-center">
           <img src={logo} alt="Logo" className="h-12 mr-4" />
--- larrycontracting/src/Components/MainPage.jsx
+++ larrycontracting/src/Components/MainPage.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
-import background from "../img/homeImage.jpg"
+import React from 'react';
+import background from '../img/homeImage.jpg';
 
 const MainPage = () => {
-    const backgroundStyle = {
-        backgroundImage: `url(${background})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-      };
+  const backgroundStyle = {
+    backgroundImage: `url(${background})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh', // 100% of the viewport height
+  };
+
+  const textStyle = {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: '26px', // Adjusted font size to 26 pixels
+  };
+
+  const lineStyle = {
+    borderBottom: '2px solid white',
+    width: '50%', // Adjust the width of the line as needed
+    margin: '20px auto', // Adjust the margin as needed
+    opacity: '80%'
+  };
 
   return (
     <div name="Home" className="w-full h-screen" style={backgroundStyle}>
+      <div>
+        <h1 style={textStyle}>LARRYS GENERAL CONSTRUCTION LLC</h1>
+        <h1 style={textStyle} className="opacity-70">Your Local General Contractor</h1>
+        <div style={lineStyle}></div>
+        <h1 className="w-[85%] m-auto" style={textStyle}>
+          Get In Touch With Your Local General Contractor For your Next Home Improvements, Upgrades, Or Repairs!
+        </h1>
+        {/* Add any other elements or images here */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
