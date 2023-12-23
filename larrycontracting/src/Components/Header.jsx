6dc4e73d0a757a5d2808734efb416a721e22297c
William Tikhonenko <williamtikhonenko@gmail.com>
2023-12-23 13:27:18 -0500

    added a basic nav bar just would like a hamburger.

--- larrycontracting/src/Components/Header.jsx
+++ larrycontracting/src/Components/Header.jsx
@@ -1,2 +1,78 @@
- {/* needs to have 5 links total Home, About Us, Our Services,
-Gallery, and Contact us*/}
+import React from 'react';
+import bgphoto from "../img/woodgrain.jpg";
+
+const Header = () => {
+  const backgroundStyle = {
+    backgroundImage: `url(${bgphoto})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    width: '100%',
+    height: '100%',
+  };
+
+  const listItemStyle = {
+    transition: 'transform 0.3s ease-in-out',
+  };
+
+  const handleHover = (event) => {
+    event.target.style.transform = 'scale(1.1)';
+  };
+
+  const handleLeave = (event) => {
+    event.target.style.transform = 'scale(1)';
+  };
+
+  return (
+    <div className="bg-cover text-white" style={backgroundStyle}>
+      <ul className="flex space-x-4 p-4">
+        <li
+          style={listItemStyle}
+          className="text-black font-bold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}
+        >
+          Home
+        </li>
+        <span className="text-gray-400">|</span>
+        <li
+          style={listItemStyle}
+          className="text-black font-bold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}
+        >
+          About Us
+        </li>
+        <span className="text-gray-400">|</span>
+        <li
+          style={listItemStyle}
+          className="text-black font-bold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}
+        >
+          Our Services
+        </li>
+        <span className="text-gray-400">|</span>
+        <li
+          style={listItemStyle}
+          className="text-black font-bold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}
+        >
+          Gallery
+        </li>
+        <span className="text-gray-400">|</span>
+        <li
+          style={listItemStyle}
+          className="text-black font-bold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}
+        >
+          Contact Us
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
---
+++ larrycontracting/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      'xs': '475px',
+      'xxs': '250px',
+      ...defaultTheme.screens,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
