305eb3e2821d2a72065f959d041ffe2f6df0b862
William Tikhonenko <williamtikhonenko@gmail.com>
2023-12-23 14:22:33 -0500

    added some more styling and LGC LLC

--- larrycontracting/src/Components/Header.jsx
+++ larrycontracting/src/Components/Header.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import bgphoto from "../img/woodgrain.jpg";
-import logo from "../img/tools.png";
+import logo from '../img/tools.png';
 
 const Header = () => {
-  const backgroundStyle = {
-    backgroundImage: `url(${bgphoto})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    width: '100%',
-    height: '100%',
-  };
-
   const listItemStyle = {
     transition: 'transform 0.3s ease-in-out',
   };
 
   const dividerStyle = {
-    opacity: 0.7, // Adjust opacity as needed
-    color: '#000', // Set color to black
-    marginTop: '3px', // Adjust margin-top as needed
+    opacity: 0.7,
+    color: '#000',
+    marginTop: '3px',
   };
 
   const handleHover = (event) => {
@@ -31,56 +21,59 @@ const Header = () => {
   };
 
   return (
-    <div className="bg-cover text-white" style={backgroundStyle}>
-      <div className="flex items-center p-4">
+    <div className="flex items-center justify-between p-4">
+      <div className="flex items-center">
         <img src={logo} alt="Logo" className="h-12 mr-4" />
-        <ul className="flex space-x-4">
-          <li
-            style={listItemStyle}
-            className="text-black font-semibold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
-            onMouseEnter={handleHover}
-            onMouseLeave={handleLeave}
-          >
-            Home
-          </li>
-          <span style={dividerStyle}>|</span>
-          <li
-            style={listItemStyle}
-            className="text-black font-semibold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
-            onMouseEnter={handleHover}
-            onMouseLeave={handleLeave}
-          >
-            About Us
-          </li>
-          <span style={dividerStyle}>|</span>
-          <li
-            style={listItemStyle}
-            className="text-black font-semibold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
-            onMouseEnter={handleHover}
-            onMouseLeave={handleLeave}
-          >
-            Our Services
-          </li>
-          <span style={dividerStyle}>|</span>
-          <li
-            style={listItemStyle}
-            className="text-black font-semibold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
-            onMouseEnter={handleHover}
-            onMouseLeave={handleLeave}
-          >
-            Gallery
-          </li>
-          <span style={dividerStyle}>|</span>
-          <li
-            style={listItemStyle}
-            className="text-black font-semibold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
-            onMouseEnter={handleHover}
-            onMouseLeave={handleLeave}
-          >
-            Contact Us
-          </li>
-        </ul>
+        <div>
+          <p className="text-black text-lg font-bold">LGC LLC</p>
+        </div>
       </div>
+      <ul className="flex space-x-4 ml-auto">
+        <li
+          style={listItemStyle}
+          className="text-black font-semibold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}
+        >
+          Home
+        </li>
+        <span style={dividerStyle}>|</span>
+        <li
+          style={listItemStyle}
+          className="text-black font-semibold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}
+        >
+          About Us
+        </li>
+        <span style={dividerStyle}>|</span>
+        <li
+          style={listItemStyle}
+          className="text-black font-semibold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}
+        >
+          Our Services
+        </li>
+        <span style={dividerStyle}>|</span>
+        <li
+          style={listItemStyle}
+          className="text-black font-semibold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}
+        >
+          Gallery
+        </li>
+        <span style={dividerStyle}>|</span>
+        <li
+          style={listItemStyle}
+          className="text-black font-semibold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}
+        >
+          Contact Us
+        </li>
+      </ul>
     </div>
   );
 };
--- larrycontracting/src/Components/MainPage.jsx
+++ larrycontracting/src/Components/MainPage.jsx
@@ -1,1 +1,10 @@
- {/* Needs to include logo and slight breif of the company */}
+import React from 'react'
+import background from "../img"
+
+const MainPage = () => {
+  return (
+    <div>MainPage</div>
+  )
+}
+
+export default MainPage
