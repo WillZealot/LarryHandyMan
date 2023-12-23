a4783dc16d1e0dd2977d55934bfd96f577d80e7f
William Tikhonenko <williamtikhonenko@gmail.com>
2023-12-23 14:05:32 -0500

    added slightly more styling to the header i really need to move on haha

--- larrycontracting/src/Components/Header.jsx
+++ larrycontracting/src/Components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import bgphoto from "../img/woodgrain.jpg";
+import logo from "../img/tools.png";
 
 const Header = () => {
   const backgroundStyle = {
@@ -15,6 +16,12 @@ const Header = () => {
     transition: 'transform 0.3s ease-in-out',
   };
 
+  const dividerStyle = {
+    opacity: 0.7, // Adjust opacity as needed
+    color: '#000', // Set color to black
+    marginTop: '3px', // Adjust margin-top as needed
+  };
+
   const handleHover = (event) => {
     event.target.style.transform = 'scale(1.1)';
   };
@@ -25,52 +32,55 @@ const Header = () => {
 
   return (
     <div className="bg-cover text-white" style={backgroundStyle}>
-      <ul className="flex space-x-4 p-4">
-        <li
-          style={listItemStyle}
-          className="text-black font-bold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
-          onMouseEnter={handleHover}
-          onMouseLeave={handleLeave}
-        >
-          Home
-        </li>
-        <span className="text-gray-400">|</span>
-        <li
-          style={listItemStyle}
-          className="text-black font-bold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
-          onMouseEnter={handleHover}
-          onMouseLeave={handleLeave}
-        >
-          About Us
-        </li>
-        <span className="text-gray-400">|</span>
-        <li
-          style={listItemStyle}
-          className="text-black font-bold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
-          onMouseEnter={handleHover}
-          onMouseLeave={handleLeave}
-        >
-          Our Services
-        </li>
-        <span className="text-gray-400">|</span>
-        <li
-          style={listItemStyle}
-          className="text-black font-bold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
-          onMouseEnter={handleHover}
-          onMouseLeave={handleLeave}
-        >
-          Gallery
-        </li>
-        <span className="text-gray-400">|</span>
-        <li
-          style={listItemStyle}
-          className="text-black font-bold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
-          onMouseEnter={handleHover}
-          onMouseLeave={handleLeave}
-        >
-          Contact Us
-        </li>
-      </ul>
+      <div className="flex items-center p-4">
+        <img src={logo} alt="Logo" className="h-12 mr-4" />
+        <ul className="flex space-x-4">
+          <li
+            style={listItemStyle}
+            className="text-black font-semibold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
+            onMouseEnter={handleHover}
+            onMouseLeave={handleLeave}
+          >
+            Home
+          </li>
+          <span style={dividerStyle}>|</span>
+          <li
+            style={listItemStyle}
+            className="text-black font-semibold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
+            onMouseEnter={handleHover}
+            onMouseLeave={handleLeave}
+          >
+            About Us
+          </li>
+          <span style={dividerStyle}>|</span>
+          <li
+            style={listItemStyle}
+            className="text-black font-semibold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
+            onMouseEnter={handleHover}
+            onMouseLeave={handleLeave}
+          >
+            Our Services
+          </li>
+          <span style={dividerStyle}>|</span>
+          <li
+            style={listItemStyle}
+            className="text-black font-semibold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
+            onMouseEnter={handleHover}
+            onMouseLeave={handleLeave}
+          >
+            Gallery
+          </li>
+          <span style={dividerStyle}>|</span>
+          <li
+            style={listItemStyle}
+            className="text-black font-semibold text-lg hover:cursor-pointer transition duration-300 ease-in-out"
+            onMouseEnter={handleHover}
+            onMouseLeave={handleLeave}
+          >
+            Contact Us
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
