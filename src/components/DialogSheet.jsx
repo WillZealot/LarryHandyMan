e378c9c08c4e31f8ea51fc2319e866897f84ab90
Nick <nicksweetj@gmail.com>
2024-01-19 00:20:05 -0500

    styled contact dialog page

--- src/components/DialogSheet.jsx
+++ src/components/DialogSheet.jsx
@@ -1,5 +1,6 @@
 import * as AlertDialog from "@radix-ui/react-alert-dialog";
 import { motion } from "framer-motion";
+import ICON from "../assets/icons/tools.png";
 
 const slideIn = {
   hidden: { x: "100%" },
@@ -7,21 +8,12 @@ const slideIn = {
 };
 
 const DialogSheet = ({ title, description, onClose }) => (
-  <AlertDialog.Root open={onClose} onOpenChange={onClose}>
+  <AlertDialog.Root defaultOpen={!onClose} open={onClose} onOpenChange={onClose}>
     <AlertDialog.Content
       as={motion.div}
       animate="visible"
       variants={slideIn}
-      style={{
-        width: "33%",
-        position: "fixed",
-        top: 0,
-        right: 0,
-        height: "100%",
-        backgroundColor: "white",
-        padding: "1em",
-        paddingTop: "7em",
-      }}
+      className="fixed top-0 right-0 h-full w-full p-4 pt-28 z-10 bg-slate-500 md:w-1/3"
     >
       <AlertDialog.Title
         style={{
@@ -29,17 +21,21 @@ const DialogSheet = ({ title, description, onClose }) => (
           fontWeight: "bold",
           padding: "15px",
           margintop: "20px",
+          color: "white",
+          textAlign: "center"
         }}
-        className="text-slate-700"
       >
         {title}
       </AlertDialog.Title>
       <AlertDialog.Description
-        style={{ marginTop: "2em", fontSize: "1.0em", padding: "20px" }}
+        style={{ marginTop: "2em", fontSize: "1.5em", padding: "10px", color: "white", textAlign: "center"}}
       >
         {description}
       </AlertDialog.Description>
-      <AlertDialog.Cancel onClick={onClose}>Close</AlertDialog.Cancel>
+      <img src={ICON} alt="logo" className="mx-auto h-32 w-32"/>
+      <div className="text-center pt-10 pb-10">
+      <AlertDialog.Cancel onClick={onClose} className="text-slate-500 bg-white p-2 pr-6 pl-6 rounded-full hover:bg-gray-200">Close</AlertDialog.Cancel>
+      </div>
     </AlertDialog.Content>
   </AlertDialog.Root>
 );
